452d82f12739ecb9db421861123f797d78acd473
Yobhani <yobhani.martinez@gmail.com>
2021-05-25 22:15:22 -0700

    working with props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const day = props.date.toLocaleString("es-MX", { day: "2-digit" });
+  const month = props.date.toLocaleString("es-MX", { month: "long" });
+  const year = props.date.getFullYear();
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+    <div className="expense-item">
+      <div>{day}-</div>
+      <div>{month}-</div>
+      <div>{year}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
